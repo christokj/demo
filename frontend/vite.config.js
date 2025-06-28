92529b1b8952bbbd776a710d5632b94306478d1f
Christo K J <christokj2@gmail.com>
2025-06-28 12:53:12 +0530

    asdfagfsg

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // Use './' if you get blank page
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
