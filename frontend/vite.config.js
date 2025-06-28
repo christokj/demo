8cbba14446a29489297ccd1fea82f8eb8edddf02
Christo K J <christokj2@gmail.com>
2025-06-28 10:47:52 +0530

    dasf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist'  // ✅ This is the default, so you could remove this line
   }
