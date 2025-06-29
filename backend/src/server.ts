6c3a9e728f3506a0d18c9384c882f2fb593bb7f7
Christo K J <christokj2@gmail.com>
2025-06-29 23:59:59 +0530

    dasfa

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 const app = express();
 
 // Enable CORS
-app.use((req: Request, res: Response, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
